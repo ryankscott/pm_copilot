7b55963b7472554b60f8c5501910a64ae1c77b64
Ryan Scott <ryankennethscott@gmail.com>
2025-07-13 22:29:20 +1200

    Fix syntax error and regenerate

---
+++ backend/src/generated/models/PRDContent.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PRDContent = {
+    /**
+     * The title of the PRD
+     */
+    title?: string;
+    sections?: Array<{
+        title?: string;
+        content?: string;
+    }>;
+    /**
+     * A summary of the PRD
+     */
+    summary?: string;
+};
+
